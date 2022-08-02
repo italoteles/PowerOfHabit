99d80f30ae6e5ce3cb4a0fc9bdd8eee49c43e5cf
italoteles <italop.teles@gmail.com>
2022-08-02 07:26:11 -0300

    RolesController has been developed

--- PowerOfHabit.API/Controllers/RolesController.cs
+++ PowerOfHabit.API/Controllers/RolesController.cs
@@ -25,5 +25,57 @@ namespace PowerOfHabit.API.Controllers
             }
             return Ok(roles);
         }
+
+        [HttpGet("{id:int}", Name = "GetRoleById")]
+        public async Task<ActionResult<RoleDTO>> Get(int id)
+        {
+            var role = await _roleService.GetByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound("Role not found");
+            }
+            return Ok(role);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] RoleDTO roleDTO)
+        {
+            if (roleDTO == null)
+            {
+                return BadRequest("Invalid Data");
+            }
+            await _roleService.CreateAsync(roleDTO);
+
+            return new CreatedAtRouteResult("GetRoleById", new { id = roleDTO.RoleId }, roleDTO);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id, [FromBody] RoleDTO roleDTO)
+        {
+            if (id != roleDTO.RoleId)
+            {
+                return BadRequest("Invalid Id");
+            }
+            if (roleDTO == null)
+            {
+                return BadRequest("Invalid Data");
+            }
+            await _roleService.UpdateAsync(roleDTO);
+
+            return Ok(roleDTO);
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<RoleDTO>> Delete(int id)
+        {
+            var role = await _roleService.GetByIdAsync(id);
+            if(role == null)
+            {
+                return NotFound("Role not found");
+            }
+            await _roleService.DeleteAsync(id);
+            return Ok(role);
+        }
     }
 }
