5729dc9bab0cead9929ecdca0dbba821cf2feec9
italoteles <italop.teles@gmail.com>
2022-08-10 11:52:05 -0300

    Authentication models

---
+++ PowerOfHabit.API/DTOs/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PowerOfHabit.API.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+        
+
+    }
+}
